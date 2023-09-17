4d7af05e196c87d36a6e0f540ca4701158b63f3f
Burak Sümer <buraksumer13@gmail.com>
2023-09-17 17:02:06 +0300

    Upgrade Astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -23,9 +20,6 @@ export default defineConfig({
   site: "https://burak.mulayim.app",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     sitemap(),
     // prefetch(),
     robotsTxt(),
